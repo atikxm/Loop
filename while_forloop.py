4a55c08ca04787561c6bf187f9844c877dfe6983
atikxm <atikurrahmanxm@gmail.com>
2025-06-04 03:50:25 +0530

    list range string example

--- while_forloop.py
+++ while_forloop.py
@@ -1,9 +1,38 @@
+#while loop example
 '''i =1 
 while i <=10:
     print ("Number is :",i)
     i += 1
     '''
+
 #infinite loop hobe
+'''
 i = 1
 while i <= 5:
     print(i)
+    '''
+
+#for loop example
+'''
+for i in range(5):
+    print(i)
+'''
+#range function example
+'''
+
+for i in range(5):
+    print(i)
+
+'''
+#list example
+'''
+fruits = ["apple", "banana", "lichu"]
+for fruit in fruits:
+    print("Fruit:", fruit)
+'''
+#string example
+'''
+name = "Alien"
+for letter in name:
+    print(letter)
+'''
